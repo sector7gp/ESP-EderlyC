4876037df8dee1fbd5db6f3dd9f80525914908fb
Pablo Gon <sector7gp@gmail.com>
2021-02-10 22:42:50 -0300

    v0.3
    config portal

--- Ederly/Ederly.ino
+++ Ederly/Ederly.ino
@@ -7,179 +7,224 @@
    TOFIX: the mac address leading Zeros..
 */
 
-//#define LED 2
-#define SENSOR 2
+#define CONFIG 2
 #define DEBUG
+#define TIMEOUT 60
+#include <FS.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include "secrets.h"
+#include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson
+#include <WiFiManager.h>
 
-char ssid[] = SECRET_SSID;   // your network SSID (name)
-char pass[] = SECRET_PASS;   // your network password
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
+#ifdef ESP32
+#include <SPIFFS.h>
+#endif
+
+//define your default values here, if there are different values in config.json, they are overwritten.
+char server[40] = "http://data.sector7gp.com/save.php";
+char port[6]  = "80";
+char clientId[5] = "0";
+
+
+//default custom static IP
+char static_ip[16] = "192.168.1.180";
+char static_gw[16] = "192.168.1.1";
+char static_sn[16] = "255.255.255.0";
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
 byte mac[6];
 String ID = "";
 WiFiClient  client;
+WiFiManager wm;
 
 
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
 
+void setupSpiffs() {
+  //clean FS, for testing
+  // SPIFFS.format();
 
-//Your Domain name with URL path or IP address with path
-const char* serverName = SERVER_NAME;
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(server, json["server"]);
+          strcpy(port, json["port"]);
+          strcpy(clientId, json["clientId"]);
+
+          if (json["ip"]) {
+            Serial.println("setting custom ip from config");
+            strcpy(static_ip, json["ip"]);
+            strcpy(static_gw, json["gateway"]);
+            strcpy(static_sn, json["subnet"]);
+            Serial.println(static_ip);
+          } else {
+            Serial.println("no custom ip in config");
+          }
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+}
 
 void setup() {
 
-  wifi_status_led_uninstall();
-  
-#ifdef LED
-  pinMode(LED, OUTPUT);
-#endif
-
-#ifdef SENSOR
-  // PIR Motion Sensor mode INPUT_PULLUP
-  pinMode(SENSOR, INPUT_PULLUP);
-#endif
-
-
-  stat(2);
-
-  IPAddress ip(192, 168 , 1 , 249);
-  IPAddress dns(8, 8, 8, 8);
-  IPAddress subnet(255, 255, 255, 0);
-  IPAddress gateway(192, 168, 1, 1);
-  WiFi.config(ip, dns, gateway, subnet);
-
-  WiFi.macAddress(mac);
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  //WiFi.macAddress(mac);
   Serial.begin(115200);  // Initialize serial
+  Serial.println(ESP.getChipId());
+  wifi_status_led_uninstall();
+  //reset settings - for testing
+  //wm.resetSettings();
+  wm.setMinimumSignalQuality(40);
 
+  setupSpiffs();
 
-  Serial.println("");
-  //Sets the chip ID
-  Serial.print("ID: ");
-  if (mac[3] < 10) ID = "0";
-  ID = ID + String (mac[3], HEX) + String( + mac[4], HEX) + String(mac[5], HEX);
-  ID.toUpperCase();
-  Serial.println(ID);
+  //set config save notify callback
+  wm.setSaveConfigCallback(saveConfigCallback);
 
-}
+  // setup custom parameters
+  //
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_server("server", "URL", server, 40);
+  WiFiManagerParameter custom_port("port", "puerto", port, 6);
+  WiFiManagerParameter custom_clientId("id", "ID Cliente", clientId, 32);
 
-//Reset function
-//void(* resetFunc) (void) = 0; //declare reset function @ address 0
+  //add all your parameters here
+  wm.addParameter(&custom_server);
+  wm.addParameter(&custom_port);
+  wm.addParameter(&custom_clientId);
 
-void loop() {
-  stat(3);
-  if (1) {
-    checkOnline();
+//  //  //set static ip
+//  IPAddress _ip, _gw, _sn;
+//  _ip.fromString(static_ip);
+//  _gw.fromString(static_gw);
+//  _sn.fromString(static_sn);
+//  wm.setSTAStaticIPConfig(_ip, _gw, _sn);
 
-    HTTPClient http;
+  wm.setConfigPortalTimeout(TIMEOUT);
+  //wm.setConnectTimeout(30);
+  //if pressed, starts the configPortal
+  delay(1000);
+  if ( digitalRead(CONFIG) == LOW) {
+    // set configportal timeout
 
-    String toPost = "{\"clientId\":\"2\",\"deviceId\":\"" + ID + "\",\"value\":\"" + "1" + "\"}";
+    wm.startConfigPortal("", "agetech0");
+  }
+  wm.autoConnect("", "agetech0");
 
-    // Your Domain name with URL path or IP address with path
-    http.begin(client, serverName);
-    http.addHeader("Content-Type", "application/json");
-    int httpResponseCode = http.POST(toPost);
+  //  if (wm.configPortalHasTimeout()) {
+  //    ESP.deepSleep(0);
+  //  }
 
-#ifdef DEBUG
-    Serial.println(toPost);
-#endif
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+  //read updated parameters
+  strcpy(server, custom_server.getValue());
+  strcpy(port, custom_port.getValue());
+  strcpy(clientId, custom_clientId.getValue());
 
-    if (httpResponseCode != 200) {
-      stat(3);
-    } else {
-      stat(1);
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["server"] = server;
+    json["port"]   = port;
+    json["clientId"]   = clientId;
+
+//    json["ip"]          = WiFi.localIP().toString();
+//    json["gateway"]     = WiFi.gatewayIP().toString();
+//    json["subnet"]      = WiFi.subnetMask().toString();
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
     }
 
-    // Free resources
-    http.end();
+    json.prettyPrintTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+    shouldSaveConfig = false;
+  }
+
+
+  Serial.println("local ip");
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.gatewayIP());
+  Serial.println(WiFi.subnetMask());
+}
+
+
+void loop() {
+  byte tries = 0;
+  HTTPClient http;
+  //ID = wm.getDefaultAPName();
+  ID = wm.getDefaultAPName().substring(4, 6);
+  String toPost = "{\"clientId\":\"2\",\"deviceId\":\"" + ID + "\",\"value\":\"" + "1" + "\"}";
+
+  // Your Domain name with URL path or IP address with path
+  Serial.println(server);
+  http.begin(client, server);
+  http.addHeader("Content-Type", "application/json");
+  int httpResponseCode = http.POST(toPost);
+
+#ifdef DEBUG
+  Serial.print("Data to Post: ");
+  Serial.println(toPost);
+#endif
+
+  while ((httpResponseCode != 200) && (tries < 5)) {
+    httpResponseCode = http.POST(toPost);
+    tries++;
+    delay(2000);
 
 #ifdef DEBUG
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
-    Serial.print("Sensor: ");
-    Serial.println(digitalRead(SENSOR));
-    Serial.println("Deep Sleep");
 #endif
 
-    ESP.deepSleep(0);
-    delay(100);
-  }
-}
-
-
-void checkOnline() {
-  // Connect or reconnect to WiFi
-  char timeOut = 0;
-  //WiFi.setOutputPower(20.5); //reduces the output power radio
-  WiFi.mode(WIFI_STA);
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Attempting to connect to: ");
-    Serial.println(SECRET_SSID);
-    while (WiFi.status() != WL_CONNECTED) {
-      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
-      Serial.print(".");
-      stat(0);
-      delay(5000);
-      if (++timeOut > 5) {
-        WiFi.disconnect();
-        Serial.print("Fail to connect");
-        Serial.println("Deep Sleep");
-        ESP.deepSleep(0);
-        delay(100);
-      }
-    }
-    Serial.print("Connected: ");
-    Serial.println(WiFi.localIP());
   }
 
-}
-/* DOS destellos cortos, intentando conectar al WiFi
-   DOS LARGOS error al envio
-   TRES destellos cortos envio correcto
-   CUATRO CORTOS secuencia de enciendido
-   DOS LARGOS error al envio
-*/
-void stat(byte _s) {
-#ifdef LED
-  int t = 50;
-  switch (_s) {
-    case 0:
-      for (int i = 0; i < 2 ; i++) {
+  // Free resources
+  http.end();
 
-        digitalWrite(LED, HIGH);
-        delay(t);
-        digitalWrite(LED, LOW);
-        delay(t);
-      }
-      break;
-    case 1:
-      for (int i = 0; i < 3 ; i++) {
-        digitalWrite(LED, HIGH);
-        delay(t);
-        digitalWrite(LED, LOW);
-        delay(t);
-      }
-      break;
-    case 2:
-      for (int i = 0; i < 4 ; i++) {
-        digitalWrite(LED, HIGH);
-        delay(t);
-        digitalWrite(LED, LOW);
-        delay(t);
-      }
-      break;
-    case 3:
-      for (int i = 0; i < 2 ; i++) {
-        digitalWrite(LED, HIGH);
-        delay(100);
-        digitalWrite(LED, LOW);
-        delay(100);
-      }
-      break;
-    default:
-      //nothing
-      break;
-  }
+#ifdef DEBUG
+  Serial.println("Deep Sleep");
 #endif
 
+  ESP.deepSleep(0);
 }
